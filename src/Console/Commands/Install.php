49b62d32d692d24ac0b75cbf708a3b0e476d93f6
Rafael Casachi <rcasachi@gmail.com>
2018-11-01 11:54:42 -0300

    Fix issues on install command

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -57,9 +57,22 @@ class Install extends Command
       $this->line('If you accept install this GelbFramework Blueprint, the follow files will be deleted and replaced for Gelb files.');
 
       $this->table(
-        ['File', 'Address'],
+        ['File', 'Address', 'Details'],
         [
-          ['file'=>'arquivo1.php', 'address'=>'./app/app/app']
+          ['file' => '.env', 'address' => './', 'details' => 'Generate a new key'],
+          ['file' => 'ExampleComponent.vue', 'address' => './resources/js/components', 'details' => 'Delete file and components folder'],
+          ['file' => 'app.js', 'address' => './resources/js', 'details' => 'Change file dependencies'],
+          ['file' => 'bootstrap.js', 'address' => './resources/js', 'details' => 'Change file dependencies'],
+          ['file' => '_variables.scss', 'address' => './resources/sass', 'details' => 'Change file code'],
+          ['file' => 'app.scss', 'address' => './resources/sass', 'details' => 'Change file code'],
+          ['file' => 'email.blade.php', 'address' => './resources/views/auth/passwords', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'reset.blade.php', 'address' => './resources/views/auth/passwords', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'login.blade.php', 'address' => './resources/views/auth', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'register.blade.php', 'address' => './resources/views/auth', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'verify.blade.php', 'address' => './resources/views/auth', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'app.blade.php', 'address' => './resources/views/layouts', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'home.blade.php', 'address' => './resources/views', 'details' => 'Change file code: adapt to visual design'],
+          ['file' => 'welcome.blade.php', 'address' => './resources/views', 'details' => 'Delete file'],
         ]
       );
     }
@@ -71,7 +84,7 @@ class Install extends Command
 
       $this->line('');
       $this->line('Gelb is generating Laravel auth classes.');
-      Artisan::call('make:auth');
+      Artisan::call('make:auth', ['--force']);
       $this->info('Laravel Auth is set.');
 
       // $codeKeyGenerate = Artisan::call('key:generate');
